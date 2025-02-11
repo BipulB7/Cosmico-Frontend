f13cd04a7d45928dd1c02bf329087fd79c46dad9
Bipul B <bipulb@Bipuls-MacBook-Pro.local>
2025-02-11 03:17:18 +0100

    Fresh start: add new frontend files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-gray-800 min-h-screen flex items-center justify-center text-white">
+      <h1 className="text-3xl font-bold">Hello, Cosmico!</h1>
+    </div>
+  );
+}
+
+export default App;
